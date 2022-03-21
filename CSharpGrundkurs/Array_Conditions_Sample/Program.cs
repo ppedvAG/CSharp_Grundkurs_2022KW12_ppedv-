e14d54f99c119ad427cfac8ef71454654ac09872
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2022-03-21 12:20:36 +0100

    Modul03 -> Array

---
+++ CSharpGrundkurs/Array_Conditions_Sample/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace Array_Conditions_Sample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Einfaches Array
+            //einfaches Array
+
+            //Deklaration mit Initialisierung
+            int[] zahlen = { 2, 4, 8, -123, -8, 0, 1111 };
+
+
+            //Zugriff via Index auf Array
+            Console.WriteLine(zahlen[3]); //Index 3 -> -123 
+
+            zahlen[5] = 1234;
+            Console.WriteLine(zahlen[5]);
+
+            #endregion
+
+            #region string-Array -> Methoden mit using System.Linq
+            string[] worte = new string[8]; //Array hat 8 Felder und noch keine zugewiesenen Werte (alle sind via default-> 0 oder bei string -> string.Empty oder auch "") 
+
+
+            //Verwenden die Contains-Methode mithilfe -> using System.Linq;
+            Console.WriteLine(zahlen.Contains(-123));
+            Console.WriteLine(zahlen.Contains(99));
+
+            Console.WriteLine(zahlen.Length); //Anzahl der Einträge
+            #endregion
+
+            #region string/char
+            //String als char-array
+
+            string beispiel = "Hallo";
+            Console.WriteLine(beispiel[2]); //l wird ausgegeben (Index = 2) 
+            #endregion
+
+
+            #region Mehrdimensionales Array
+
+            int pos = 2;
+            int[,] zweiDimArray = new int[pos, 5];
+
+            int[] test = new int[pos];
+
+
+            
+            #endregion
+        }
+    }
+}
---
+++ CSharpGrundkurs/Modul02_LabLoesung/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Modul02_LabLoesung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variablendeklaration 
+            int entfernungInMetern, stunden, minuten, sekunden;
+            double meterProSekunde, kilometerProStunde, meilenProStunde;
+
+
+            Console.WriteLine("Bitte gib folgende Angabe ein: ");
+            Console.Write("Entfernung (in Metern): ");
+
+            //parsen der Eingabe in Int32 (int) 
+            entfernungInMetern = int.Parse(Console.ReadLine());
+            Console.Write("Stunden: ");
+            
+            stunden = int.Parse(Console.ReadLine());
+            Console.Write("Minuten: ");
+
+            minuten = int.Parse(Console.ReadLine());
+            Console.Write("Sekunden: ");
+            sekunden = int.Parse(Console.ReadLine());
+
+            //Ausgabe einer leeren Zeile
+            Console.WriteLine();
+
+            sekunden = sekunden + (minuten * 60) + (stunden * 3600);
+            meterProSekunde = entfernungInMetern / (double)sekunden;
+            kilometerProStunde = meterProSekunde * 3.6;
+            meilenProStunde = kilometerProStunde * 0.62137119;
+
+            Console.WriteLine($"Meter/Sekunde:\t\t {Math.Round(meterProSekunde)}");
+            Console.WriteLine($"Kilometer/Stunde:\t {Math.Round(kilometerProStunde)}");
+            Console.WriteLine($"Meile/Stunde:\t\t {Math.Round(meilenProStunde)}");
+        }
+    }
+}
